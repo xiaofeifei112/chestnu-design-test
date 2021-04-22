38149c1a5c7dccea57a4657fd264985db0abda24
feifei <lingfei.qiu@gemii.cc>
2021-04-23 03:23:17 +0800

    优化 样式引入问题，打包完成插件大小 101kb

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 import './styles/iconfont.css';
+import './styles/animate.scss';
 export { default as ButtonBox } from './ButtonBox';
 export { default as AvatarGroup } from "./AvatarGroup";
 export { default as BreadCrumb } from "./BreadCrumb";
